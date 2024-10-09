e172fe8b4e0e270c5dc75f49aa84652509ec506e
Huy Nguyen <ng.q.huy98@gmail.com>
2024-10-09 13:42:52 +0300

    basic comments service

--- service_comments/src/app.ts
+++ service_comments/src/app.ts
@@ -8,15 +8,31 @@ app.use(bodyParser.json());
 
 interface Comment {
   id: string;
-
+  content: string;
 }
 
+// Map between postID and comment object
+// For example: "post1": [{ comment_1 }, { comment_2 }]
+const commentsByPostId: Map<string, Comment[]> = new Map<string, Comment[]>();
+
 app.get('/posts/:id/comments', (req, res) => {
-  res.send('Hello, TypeScript Node Express!');
+  const comments = commentsByPostId.get(req.params.id) || [];
+  console.log(commentsByPostId);
+  console.log(comments);
+  res.send(comments);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
-  res.send('Hello, TypeScript Node Express!');
+  const id = randomBytes(4).toString('hex');
+  const { content } = req.body;
+  const newComment = { id: id, content: content }
+  
+  const comments = commentsByPostId.get(req.params.id) || [];
+  comments.push({ id: id, content: content });
+  commentsByPostId.set(req.params.id, comments);
+
+  res.status(201);
+  res.send(newComment);
 });
 
 app.listen(port, () => {
