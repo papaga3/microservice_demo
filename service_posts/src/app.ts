3ff6e73b765cb402b71f930c22c681f594645c12
Huy Nguyen <ng.q.huy98@gmail.com>
2024-10-10 08:01:08 +0300

    post service cors update

--- service_posts/src/app.ts
+++ service_posts/src/app.ts
@@ -1,25 +1,35 @@
 import express from 'express';
 import { randomBytes } from 'crypto';
 import bodyParser from 'body-parser';
+import cors, { CorsOptions } from "cors";
 
-const app = express();
-const port = 4000;
-app.use(bodyParser.json());
+
 
 interface Post {
  id: string;
  title: string; 
 }
 
+const app = express();
+const port = 4000;
+app.use(bodyParser.json());
+app.use(cors());
+
+const corsOptions: CorsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200
+}
+
 const posts: Map<string, Post> = new Map<string, Post>();
 
-app.get('/posts', (req, res) => {
+app.get('/posts', cors(corsOptions),  (req, res) => {
   res.send(JSON.stringify(Object.fromEntries(posts)));
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', cors(corsOptions), (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
+  console.log(req.body);
   const newPost = { id: id, title: title }
   posts.set(id, newPost);
   res.status(201);
